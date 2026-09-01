799e2db11dda5abaf6b7a81771253c5c288c2ce0
CCrisstian <crisstiann.c@gmail.com>
2026-09-01 12:10:37 -0300

    Mudanza a un nuevo esquema de BD y un nuevo controlador para mantener despierto el servidor

---
+++ src/main/java/com/cristian/springboot/backend/chat/app/controllers/HealthController.java
@@ -0,0 +1,16 @@
+package com.cristian.springboot.backend.chat.app.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/health")
+public class HealthController {
+
+    @GetMapping
+    public ResponseEntity<String> keepAlive() {
+        return ResponseEntity.ok("Servidor activo y despierto");
+    }
+}
